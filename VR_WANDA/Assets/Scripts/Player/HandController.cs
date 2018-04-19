779a8fc43b2abe7dd97cfb8a50ce4257ddf7c1f2
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-19 17:29:23 +0900

    Ver1.0

---
+++ VR_WANDA/Assets/Scripts/Player/HandController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public bool IsHandGripping, IsWalking;
+    public Vector3 GripPosition, ControllerVelocity;
+    public bool IsTriggered;
+    public GameObject GrippingObject;
+    [SerializeField] SteamVR_TrackedObject HandDevice;
+    [SerializeField] Renderer modelrend;
+    [SerializeField] Animator animator;
+    Rigidbody rb;
+    void Start ()
+    {
+        HandDevice = GetComponent<SteamVR_TrackedObject> ();
+        rb = GetComponent<Rigidbody> ();
+    }
+    void Update ()
+    {
+        var device = SteamVR_Controller.Input ((int) HandDevice.index);
+        IsTriggered = device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger);
+        ControllerVelocity = rb.velocity;
+        if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger))
+        {
+            IsTriggered = false;
+            IsHandGripping = false;
+            animator.SetBool ("Trigger", false);
+            modelrend.material.color = Color.white;
+        }
+        if (!IsWalking && device.GetPressDown (SteamVR_Controller.ButtonMask.Grip))
+        {
+            modelrend.material.color = Color.green;
+            IsWalking = true;
+        }
+        if (IsWalking && device.GetPressUp (SteamVR_Controller.ButtonMask.Grip))
+        {
+            IsWalking = false;
+            modelrend.material.color = Color.white;
+        }
+        if (IsTriggered)
+        {
+            modelrend.material.color = Color.blue;
+            animator.SetBool ("Trigger", true);
+        }
+    }
+
+    void OnTriggerStay (Collider collider)
+    {
+        if (!IsHandGripping)
+        {
+            if (IsTriggered && collider.tag == "GripPoint")
+            {
+                IsHandGripping = true;
+                GrippingObject = collider.gameObject;
+                GripPosition = transform.position;
+                modelrend.material.color = Color.red;
+                animator.SetBool ("Trigger", true);
+            }
+        }
+    }
+}
---
+++ VR_WANDA/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    [SerializeField]
+    Transform HMDTransform;
+    [SerializeField]
+    HandController RightDevice, LeftDevice;
+    [SerializeField]
+    float WalkSpeed;
+    Vector3 RightPosCash, LeftPosCash;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        RightPosCash = RightDevice.transform.position;
+        LeftPosCash = LeftDevice.transform.position;
+    }
+    void Update()
+    {
+        if (RightDevice.IsHandGripping && LeftDevice.IsHandGripping)
+        {
+            DowbleGrip();
+        }
+        else if (RightDevice.IsHandGripping || LeftDevice.IsHandGripping)
+        {
+            if (RightDevice.IsHandGripping)
+            {
+                SingleGrip(RightDevice);
+            }
+            else
+            {
+                SingleGrip(LeftDevice);
+            }
+        }
+        else
+        {
+            Fall();
+            if (RightDevice.IsWalking && LeftDevice.IsWalking)
+            {
+                Walk();
+            }
+        }
+    }
+    void DowbleGrip()
+    {
+        rigidbody.useGravity = false;
+        transform.parent = RightDevice.GrippingObject.transform;
+        Vector3 RightOffset = RightDevice.transform.position - RightDevice.GripPosition;
+        Vector3 LeftOffset = LeftDevice.transform.position - LeftDevice.GripPosition;
+        Vector3 AveOffset = (RightOffset + LeftOffset) / 2;
+        rigidbody.position = Vector3.Lerp(rigidbody.position, rigidbody.position - AveOffset, 0.1f);
+    }
+    void SingleGrip(HandController device)
+    {
+        rigidbody.useGravity = false;
+        transform.parent = device.GrippingObject.transform;
+        Vector3 offset = device.transform.position - device.GripPosition;
+        rigidbody.position = Vector3.Lerp(rigidbody.position, rigidbody.position - offset, 0.1f);
+    }
+    void Fall()
+    {
+        transform.parent = null;
+        rigidbody.useGravity = true;
+    }
+    void Walk()
+    {
+        float DifPosRight = (RightPosCash - RightDevice.transform.position).magnitude;
+        float DifPosLeft = (LeftPosCash - LeftDevice.transform.position).magnitude;
+        float AveVeloY = Mathf.Clamp((DifPosRight + DifPosLeft) / (2 * Time.deltaTime), 0, 3);
+        Debug.Log("Walking : " + AveVeloY);
+        Vector3 forward = new Vector3(HMDTransform.forward.x, 0, HMDTransform.forward.z).normalized;
+        rigidbody.velocity = HMDTransform.forward * WalkSpeed * AveVeloY;
+        RightPosCash = RightDevice.transform.position;
+        LeftPosCash = LeftDevice.transform.position;
+    }
+}
