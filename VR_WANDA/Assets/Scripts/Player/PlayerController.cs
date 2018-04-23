209b20b08ee8edd3c85a618ae0302a1f22501bc3
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-23 20:45:26 +0900

    Ver1.5

--- VR_WANDA/Assets/Scripts/Player/PlayerController.cs
+++ VR_WANDA/Assets/Scripts/Player/PlayerController.cs
@@ -14,79 +14,78 @@ public class PlayerController : MonoBehaviour
     Vector3 offset, RightPosCash, LeftPosCash, LocalGrippingPos;
     Collider collider;
     bool OldRightGrippingState, OldLeftGrippingState;
-    void Start ()
+    void Start()
     {
-        rigidbody = GetComponent<Rigidbody> ();
+        rigidbody = GetComponent<Rigidbody>();
         collider = GetComponent<Collider>();
         RightPosCash = RightDevice.transform.position;
         LeftPosCash = LeftDevice.transform.position;
     }
-    void Update ()
+    void Update()
     {
         if (RightDevice.IsHandGripping && LeftDevice.IsHandGripping)
         {
-            DowbleGrip ();
-            Grip ();
+            DowbleGrip();
+            Grip();
         }
         else if (RightDevice.IsHandGripping || LeftDevice.IsHandGripping)
         {
             if (RightDevice.IsHandGripping)
             {
-                SingleGrip (RightDevice);
-                Grip ();
+                SingleGrip(RightDevice);
+                Grip();
             }
             else
             {
-                SingleGrip (LeftDevice);
-                Grip ();
+                SingleGrip(LeftDevice);
+                Grip();
             }
         }
         else
         {
-            Fall ();
+            Fall();
             if (RightDevice.IsWalking && LeftDevice.IsWalking)
             {
-                Walk ();
+                Walk();
             }
         }
         OldRightGrippingState = RightDevice.IsHandGripping;
         OldLeftGrippingState = LeftDevice.IsHandGripping;
     }
-    void Grip ()
+    void Grip()
     {
         rigidbody.useGravity = false;
         collider.isTrigger = true;
-        rigidbody.position = transform.TransformPoint (LocalGrippingPos);
-        rigidbody.position = Vector3.Lerp (rigidbody.position, rigidbody.position - offset, 0.1f);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, LocalGrippingPos - offset, 0.1f);
         LocalGrippingPos = transform.localPosition;
     }
-    void DowbleGrip ()
+    void DowbleGrip()
     {
         transform.parent = RightDevice.GrippingObject.transform;
         Vector3 RightOffset = RightDevice.transform.position - RightDevice.GripPosition;
         Vector3 LeftOffset = LeftDevice.transform.position - LeftDevice.GripPosition;
         offset = (RightOffset + LeftOffset) / 2;
     }
-    void SingleGrip (HandController device)
+    void SingleGrip(HandController device)
     {
         transform.parent = device.GrippingObject.transform;
         offset = device.transform.position - device.GripPosition;
     }
-    void Fall ()
+    void Fall()
     {
         transform.parent = null;
-        rigidbody.rotation = Quaternion.Euler (0, 0, 0);
+        rigidbody.rotation = Quaternion.Euler(0, 0, 0);
         rigidbody.useGravity = true;
         collider.isTrigger = false;
     }
-    void Walk ()
+    void Walk()
     {
         rigidbody.useGravity = true;
         collider.isTrigger = false;
         float DifPosRight = (RightPosCash - RightDevice.transform.position).magnitude;
         float DifPosLeft = (LeftPosCash - LeftDevice.transform.position).magnitude;
-        float AveVeloY = Mathf.Clamp ((DifPosRight + DifPosLeft) / (2 * Time.deltaTime), 0, 3);
-        Vector3 forward = new Vector3 (HMDTransform.forward.x, 0, HMDTransform.forward.z);
+        float AveVeloY = Mathf.Clamp((DifPosRight + DifPosLeft) / (2 * Time.deltaTime), 0, 3);
+        Vector3 forward = new Vector3(HMDTransform.forward.x, 0, HMDTransform.forward.z);
         rigidbody.velocity = forward * WalkSpeed * AveVeloY;
         RightPosCash = RightDevice.transform.position;
         LeftPosCash = LeftDevice.transform.position;
