0ecdb15b4d5d24d183de16bc3a3112e774cc9b74
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-25 15:57:23 +0900

    Ver1.8

---
+++ VR_WANDA/Assets/Scripts/ShowBoneDirection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBoneDirection : MonoBehaviour
+{
+    bool HasGetChildren;
+    List<Transform> allChildrenTra = new List<Transform>();
+    void OnDrawGizmosSelected()
+    {
+        if (!HasGetChildren)
+        {
+            List<GameObject> allChildren = GetAllChildren.GetAll(gameObject);
+            foreach (GameObject g in allChildren)
+            {
+                allChildrenTra.Add(g.transform);
+            }
+            HasGetChildren = true;
+        }
+        foreach (Transform t in allChildrenTra)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(t.position, t.position + t.right);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(t.position, t.position + t.up);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(t.position, t.position + t.forward);
+        }
+    }
+}
